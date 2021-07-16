a5fb41f52340ec0e83fea7d1b5bc169036456550
Selim El Mahi <selim.elmahi@gmail.com>
2021-07-16 14:16:54 +0200

    ajout des verif js du formulaire, email non valide ou champs vide + alert d'erreur

--- js/reqPostCommand.js
+++ js/reqPostCommand.js
@@ -1,31 +1,40 @@
 var regleMail = new RegExp(/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i);
 
-
-function isValid(value,regle) {
-
+function isValid(value,regle) {         // fonction teste si une valeur respecte une ragle regex
     var valid = regle.test(value)
 
     if (valid){
         return true
     }
     else {
-        console.log("value " + value)
-        console.log("regle " + regle)
-        console.log("valid ? " + valid)
+        return false
+    }
+}
+
+function notEmpty (elm){
+    if (elm != ""){
+        return true
+    }
+    else {
         return false
     }
 }
 
 
 
+
+/* /_\ Fonction command appeller par le bouton de commande dans la page panier.html /_\ */
+
 function command(){
     var nomSaisie = document.getElementById("nom").value;                  // on récupère les valeur du formulaire
+    console.log(nomSaisie)
+    console.log(notEmpty(nomSaisie))
     var prenomSaisie = document.getElementById("prenom").value;
     var adresseSaisie = document.getElementById("adress").value;
     var villeSaisie = document.getElementById("ville").value;
     var emailSaisie = document.getElementById("mail").value;
 
-    if (isValid(adresseSaisie,regleAdress)){
+    if (isValid(emailSaisie,regleMail) && notEmpty(nomSaisie) && notEmpty(prenomSaisie) && notEmpty(adresseSaisie) && notEmpty(villeSaisie)){
 
         var productArray = JSON.parse(localStorage.getItem("cart"));            // on récupère le tableau d'id commandandé
         var infoContact = new Contact(nomSaisie, prenomSaisie, adresseSaisie, villeSaisie, emailSaisie)     // création de l'objet contact pour l'envoie
@@ -55,9 +64,24 @@ function command(){
             
         })
         .catch(function(err){
+            console.log(err)
         })    
     }
-    else {
-        console.log("formulaire non valide")
+    else {      // afficher alert en fonction de l'erreur
+
+        // email non valide et un des champs vide
+        if (!isValid(emailSaisie,regleMail) && (!notEmpty(nomSaisie) || !notEmpty(prenomSaisie) || !notEmpty(adresseSaisie) || !notEmpty(villeSaisie))){
+            alert("Veuillez saisir un email valide et remplir tout les champs du formulaire")
+        }
+
+        // un de champs vide
+        else if (!notEmpty(nomSaisie) || !notEmpty(prenomSaisie) || !notEmpty(adresseSaisie) || !notEmpty(villeSaisie)) {
+            alert("Vous avez laissé un champ du formulaire vide")
+        }
+
+        // email non valide
+        else if (!isValid(emailSaisie,regleMail)){
+            alert("Veuillez saisir un email valide")
+        }
     }
 }
